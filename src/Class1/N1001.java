1b971b672d937f0505525fb3b34f144539b73be5
이서연 <dltj2541@naver.com>
2024-10-17 21:11:53 +0900

    commit

--- src/Class1/N1001.java
+++ src/Class1/N1001.java
@@ -1,4 +1,16 @@
 package Class1;
 
+import java.util.Scanner;
+
 public class N1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 0, b = 0;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a - b);
+    }
 }
